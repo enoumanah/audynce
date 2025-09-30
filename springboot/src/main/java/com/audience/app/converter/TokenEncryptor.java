eb97909210a4bc4da704ebf16c0d4d5e31584a0b
enoumanah <141334494+enoumanah@users.noreply.github.com>
2025-09-30 20:45:39 +0100

    Day 1: Encryption and Evn var

--- springboot/src/main/java/com/audience/app/converter/TokenEncryptor.java
+++ springboot/src/main/java/com/audience/app/converter/TokenEncryptor.java
@@ -1,36 +1,48 @@
 package com.audience.app.converter;
 
 import jakarta.persistence.AttributeConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
 import java.util.Base64;
+import java.security.Key;
 
+@Component
 public class TokenEncryptor implements AttributeConverter<String, String> {
 
-    public static final String ALGORITHM = "AES";
-    public static final byte[] KEY = "your-super-secret-key-12345678".getBytes();
+    private static final String ALGORITHM = "AES";
+    private final Key secretKeySpec;
+
+    public TokenEncryptor(@Value("${audiance.security.encryptor-key}") String secretKey) {
+        byte[] keyBytes = new byte[16];
+        byte[] providedKeyBytes = secretKey.getBytes();
+        System.arraycopy(providedKeyBytes, 0, keyBytes, 0, Math.min(providedKeyBytes.length, keyBytes.length));
+        this.secretKeySpec = new SecretKeySpec(keyBytes, ALGORITHM);
+    }
 
     @Override
     public String convertToDatabaseColumn(String attribute) {
+        if (attribute == null) return null;
         try {
             Cipher c = Cipher.getInstance(ALGORITHM);
-            c.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(KEY, ALGORITHM));
+            c.init(Cipher.ENCRYPT_MODE, this.secretKeySpec);
             return Base64.getEncoder().encodeToString(c.doFinal(attribute.getBytes()));
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Error encrypting attribute", e);
         }
     }
 
     @Override
     public String convertToEntityAttribute(String dbData) {
+        if (dbData == null) return null;
         try {
             Cipher c = Cipher.getInstance(ALGORITHM);
-            c.init(Cipher.DECRYPT_MODE, new SecretKeySpec(KEY, ALGORITHM));
+            c.init(Cipher.DECRYPT_MODE, this.secretKeySpec);
             return new String(c.doFinal(Base64.getDecoder().decode(dbData)));
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Error decrypting attribute", e);
         }
     }
-
 }
